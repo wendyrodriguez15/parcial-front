0b11735aaab94a86dd28b6bdd1e72df87b4c658b
wendyrodriguez15 <yulianagarciawr@gmail.com>
2023-10-30 18:57:03 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Card from './Components/Card'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form > </form>
+      <Card />
+    </div>
+
+  );
+}
+
+export default App
